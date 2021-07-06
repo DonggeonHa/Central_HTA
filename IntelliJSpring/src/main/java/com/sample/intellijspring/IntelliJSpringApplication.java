f7fe92daa6cd856158732869f6f1028f23b32407
Hadonggeon <Hadonggun@gmail.com>
2021-07-06 17:12:06 +0900

    ADD spring/spring-Di

---
+++ IntelliJSpring/src/main/java/com/sample/intellijspring/IntelliJSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sample.intellijspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliJSpringApplication.class, args);
+    }
+
+}
---
+++ IntelliJSpring/src/test/java/com/sample/intellijspring/IntelliJSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.intellijspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliJSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
